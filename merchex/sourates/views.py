01a9971456ea88bcd28b70dc2ebbb66b7d73711c
ROUX SACHA <sacha.roux@etu.univ-lyon1.fr>
2023-12-26 00:42:24 +0100

    Création de gabarits

--- merchex/sourates/views.py
+++ merchex/sourates/views.py
@@ -3,24 +3,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from sourates.models import Band
+from sourates.models import Title
+
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request,
+        'sourates/hello.html',
+        {'bands': bands})
 
 def about(request):
-    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+    return render(request,'sourates/about.html')
 
 def sourates(request):
-    return HttpResponse('<h1>La sourate du jour</h1>')
+    titles = Title.objects.all()
+    return render(request,'sourates/sourates.html', {'titles' : titles})
 
 def contact(request):
-    return HttpResponse('<h1>Nous contacter</h1>')
+    return render(request,'sourates/contact.html')
