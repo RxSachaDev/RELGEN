8d4b11a5bd452031a39c059dbb35454ab04c3fe7
ROUX SACHA <sacha.roux@etu.univ-lyon1.fr>
2024-01-15 12:43:00 +0100

    Recherche automatique de la date dans la base données + si prière déjà passée heure de la date du lendemain

--- merchex/sourates/views.py
+++ merchex/sourates/views.py
@@ -36,9 +36,21 @@ def sourates(request):
 
     date = Date.objects.all()
 
+    now = datetime.now()
+
     current_time = datetime.now().time()
+
+    i = 0
+    while date[i].date_jour.year != now.year and date[i].date_jour.day != now.day and date[i].date_jour.month != now.month:
+        i += 1
     
-    Fajr =  datetime.combine(datetime.today(), date[0].fajr) - datetime.combine(datetime.today(), current_time)
+    time_difference = datetime.combine(datetime.today(), date[i].fajr) - datetime.combine(datetime.today(), current_time)
+
+    if time_difference.total_seconds() < 0:
+        Fajr = datetime.combine(datetime.today(), date[i+1].fajr) - datetime.combine(datetime.today(), current_time)
+    else:
+        Fajr = time_difference
+
 
     fajr_hours = Fajr.seconds // 3600
 
@@ -46,7 +58,12 @@ def sourates(request):
 
     fajr_seconds = (Fajr.seconds % 3600) % 60
 
-    Maghreb =  datetime.combine(datetime.today(), date[0].maghreb) - datetime.combine(datetime.today(), current_time)
+    time_difference2 = datetime.combine(datetime.today(), date[i].maghreb) - datetime.combine(datetime.today(), current_time)
+
+    if time_difference.total_seconds() < 0:
+        Maghreb = datetime.combine(datetime.today(), date[i+1].maghreb) - datetime.combine(datetime.today(), current_time)
+    else:
+        Fajr = time_difference2
 
     maghreb_hours = Maghreb.seconds // 3600
 
@@ -54,7 +71,13 @@ def sourates(request):
 
     maghreb_seconds = (Maghreb.seconds % 3600) % 60
 
-    Dhuhr =  datetime.combine(datetime.today(), date[0].dhuhr) - datetime.combine(datetime.today(), current_time)
+    time_difference3 = datetime.combine(datetime.today(), date[i].dhuhr) - datetime.combine(datetime.today(), current_time)
+
+    if time_difference.total_seconds() < 0:
+        Dhuhr = datetime.combine(datetime.today(), date[i+1].dhuhr) - datetime.combine(datetime.today(), current_time)
+    else:
+        Dhuhr = time_difference3
+
 
     dhuhr_hours = Dhuhr.seconds // 3600
 
@@ -62,7 +85,13 @@ def sourates(request):
 
     dhuhr_seconds = (Dhuhr.seconds % 3600) % 60
 
-    Asr =  datetime.combine(datetime.today(), date[0].asr) - datetime.combine(datetime.today(), current_time)
+    time_difference4 = datetime.combine(datetime.today(), date[i].asr) - datetime.combine(datetime.today(), current_time)
+
+    if time_difference.total_seconds() < 0:
+        Asr = datetime.combine(datetime.today(), date[i+1].asr) - datetime.combine(datetime.today(), current_time)
+    else:
+        Asr = time_difference4
+
 
     asr_hours = Asr.seconds // 3600
 
@@ -70,7 +99,13 @@ def sourates(request):
 
     asr_seconds = (Asr.seconds % 3600) % 60
 
-    Icha =  datetime.combine(datetime.today(), date[0].icha) - datetime.combine(datetime.today(), current_time)
+    time_difference5 = datetime.combine(datetime.today(), date[i].icha) - datetime.combine(datetime.today(), current_time)
+
+    if time_difference.total_seconds() < 0:
+        Icha = datetime.combine(datetime.today(), date[i+1].icha) - datetime.combine(datetime.today(), current_time)
+    else:
+        Icha = time_difference5
+
 
     icha_hours = Icha.seconds // 3600
 
