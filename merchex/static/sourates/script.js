0c1d876833ce909a146a7d95d854cee145b1b42d
ROUX SACHA <sacha.work38@gmail.com>
2024-02-07 04:24:03 +0100

    Animation JS

---
+++ merchex/static/sourates/script.js
@@ -0,0 +1,5 @@
+gsap.fromTo(".haut img", {y: 60, opacity: 0}, {y: 0, duration: 1.5, opacity: 1});
+gsap.fromTo("h1", {y: -60, opacity: 0}, {y: 0, duration: 1.5, opacity: 1});
+gsap.fromTo(".sourate h3, .sourate p, .B h2", {x: -60, opacity: 0}, {x: 0, duration: 1.5, opacity: 1, stagger: { each : 0.4}});
+gsap.fromTo(".info p", {opacity: 0}, {opacity: 1, duration: 1.5});
+gsap.fromTo(".conditions", {opacity: 0}, {opacity: 1, duration: 1.5, stagger: { each : 0.4}});
